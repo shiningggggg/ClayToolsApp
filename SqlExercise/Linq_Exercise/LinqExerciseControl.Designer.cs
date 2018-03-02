0235b8099ea77be64a1f3dd08cbc803232a73993
zhangbo <zhangbobo1234yeah@126.com>
2018-03-02 21:56:46 +0800

    C#联查datatable

---
+++ SqlExercise/Linq_Exercise/LinqExerciseControl.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SqlExercise.Linq_Exercise
+{
+    partial class LinqExerciseControl
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCreateTable = new System.Windows.Forms.Button();
+            this.btnInsertData = new System.Windows.Forms.Button();
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnNonQuery = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(16, 13);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(170, 35);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "开始联查";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(425, 408);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnCreateTable
+            // 
+            this.btnCreateTable.Location = new System.Drawing.Point(193, 13);
+            this.btnCreateTable.Name = "btnCreateTable";
+            this.btnCreateTable.Size = new System.Drawing.Size(161, 35);
+            this.btnCreateTable.TabIndex = 2;
+            this.btnCreateTable.Text = "创建表";
+            this.btnCreateTable.UseVisualStyleBackColor = true;
+            this.btnCreateTable.Click += new System.EventHandler(this.btnCreateTable_Click);
+            // 
+            // btnInsertData
+            // 
+            this.btnInsertData.Location = new System.Drawing.Point(361, 13);
+            this.btnInsertData.Name = "btnInsertData";
+            this.btnInsertData.Size = new System.Drawing.Size(154, 35);
+            this.btnInsertData.TabIndex = 3;
+            this.btnInsertData.Text = "插入数据";
+            this.btnInsertData.UseVisualStyleBackColor = true;
+            this.btnInsertData.Click += new System.EventHandler(this.btnInsertData_Click);
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Location = new System.Drawing.Point(522, 13);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(160, 35);
+            this.btnShowData.TabIndex = 4;
+            this.btnShowData.Text = "显示数据";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            this.btnShowData.Click += new System.EventHandler(this.btnShowData_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(456, 165);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 30;
+            this.dataGridView2.Size = new System.Drawing.Size(434, 408);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(909, 165);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 30;
+            this.dataGridView3.Size = new System.Drawing.Size(449, 408);
+            this.dataGridView3.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1342, 105);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "2";
+            // 
+            // btnNonQuery
+            // 
+            this.btnNonQuery.Location = new System.Drawing.Point(689, 13);
+            this.btnNonQuery.Name = "btnNonQuery";
+            this.btnNonQuery.Size = new System.Drawing.Size(158, 35);
+            this.btnNonQuery.TabIndex = 8;
+            this.btnNonQuery.Text = "执行非查询操作";
+            this.btnNonQuery.UseVisualStyleBackColor = true;
+            this.btnNonQuery.Click += new System.EventHandler(this.btnNonQuery_Click);
+            // 
+            // LinqExerciseControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnNonQuery);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnShowData);
+            this.Controls.Add(this.btnInsertData);
+            this.Controls.Add(this.btnCreateTable);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnQuery);
+            this.Name = "LinqExerciseControl";
+            this.Size = new System.Drawing.Size(1374, 603);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCreateTable;
+        private System.Windows.Forms.Button btnInsertData;
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnNonQuery;
+    }
+}
---
+++ SqlExercise/Linq_Exercise/LinqExerciseControl.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SqlExercise.Base.SqlMethod;
+
+namespace SqlExercise.Linq_Exercise
+{
+    public partial class LinqExerciseControl : UserControl
+    {
+        public LinqExerciseControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            string test = this.textBox1.Text;
+            if (test == "1")
+            {
+                DataTable headTable = SqlHelper.GetDataSet("SELECT * FROM dtHead").Tables[0];
+                DataTable tailTable = SqlHelper.GetDataSet("SELECT * FROM dtTail").Tables[0];
+                var query = from dt1 in headTable.AsEnumerable()
+                            from dt2 in tailTable.AsEnumerable()
+                            where dt1.Field<int>("GoodID") == dt2.Field<int>("GoodID")
+                            select new
+                            {
+                                GoodID = dt1.Field<Int32>("GoodID"),
+                                GoodName = dt1.Field<string>("GoodName"),
+                                Num = dt2.Field<int>("Num"),
+                                Money = dt2.Field<decimal>("Money")
+                            };
+                var list = query.ToList();
+                DataTable dtAll = CreateDataTableSchema();
+
+                foreach (var obj in query)
+                {
+                    dtAll.Rows.Add(obj.GoodID, obj.GoodName, obj.Num, obj.Money);
+                }
+                this.dataGridView3.DataSource = dtAll;
+            }
+            else if (test == "2")
+            {
+                DataTable headTable = SqlHelper.GetDataSet("SELECT * FROM dtHead").Tables[0];
+                DataTable tailTable = SqlHelper.GetDataSet("SELECT * FROM dtTail").Tables[0];
+                var query = from htable in headTable.AsEnumerable()
+                            join tTail in tailTable.AsEnumerable()
+                            on htable.Field<Int32>("GoodID") equals tTail.Field<Int32>("GoodID")
+                            select htable.ItemArray.Concat(tTail.ItemArray.Skip(1));
+                DataTable dtAll = CreateDataTableSchema();
+                foreach (var obj in query)
+                {
+                    DataRow dr = dtAll.NewRow();
+                    dr.ItemArray = obj.ToArray();
+                    dtAll.Rows.Add(dr);
+                }
+                this.dataGridView3.DataSource = dtAll;
+            }
+            else if (test == "3")
+            {
+                DataSet ds = new DataSet();
+                DataTable headTable = SqlHelper.GetDataSet("SELECT * FROM dtHead").Tables[0];
+                DataTable tailTable = SqlHelper.GetDataSet("SELECT * FROM dtTail").Tables[0];
+                DataColumn[] leftRelationCols = new DataColumn[headTable.Columns.Count];
+                //for (int i = 0; i < headTable.Columns.Count; i++)
+                    //leftRelationCols[i] = headTable.Columns[i].ColumnName;
+            }
+        }
+        private DataTable CreateDataTableSchema()
+        {
+            DataTable dtAll = new DataTable();
+            dtAll.Columns.Add("GoodID", typeof(int));
+            dtAll.Columns.Add("GoodName", typeof(string));
+            dtAll.Columns.Add("Num", typeof(int));
+            dtAll.Columns.Add("Money", typeof(decimal));
+            return dtAll;
+        }
+        private void btnCreateTable_Click(object sender, EventArgs e)
+        {
+            string sql1 = "create table dtHead (GoodID INT primary key Identity not null,GoodName nvarchar(100) null)";
+            string sql2 = "create table dtTail (GoodID INT primary key Identity not null,Num int null,Money decimal null)";
+            try
+            {
+                SqlHelper.ExecuteNonQuery(sql1);
+                SqlHelper.ExecuteNonQuery(sql2);
+                MessageBox.Show("创建成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnInsertData_Click(object sender, EventArgs e)
+        {
+            string sql = "insert into dtHead values('青岛纯生'),('哈尔滨啤酒')";
+            string sql2 = "insert into dtTail values(10,30),(5,20)";
+            try
+            {
+                SqlHelper.ExecuteNonQuery(sql);
+                SqlHelper.ExecuteNonQuery(sql2);
+                MessageBox.Show("插入成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShowData_Click(object sender, EventArgs e)
+        {
+            string sqlHead = "select * from dtHead";
+            string sqlTail = "select * from dtTail";
+            try
+            {
+                this.dataGridView1.DataSource = SqlHelper.GetDataSet(sqlHead).Tables[0];
+                this.dataGridView2.DataSource = SqlHelper.GetDataSet(sqlTail).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// 执行Sql
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNonQuery_Click(object sender, EventArgs e)
+        {
+            string sql = this.textBox1.Text;
+            if (string.IsNullOrEmpty(sql))
+            {
+                MessageBox.Show("无sql");
+                return;
+            }
+            try
+            {
+                SqlHelper.ExecuteNonQuery(sql);
+                MessageBox.Show("执行成功！");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
