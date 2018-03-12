6619641c4318205c32c8fdd25f25e29e4d82e5ac
zhangbo <zhangbobo1234yeah@126.com>
2018-03-12 20:59:24 +0800

    Procedure

---
+++ SqlExercise/Procedure/ProcedureControl.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SqlExercise.Procedure
+{
+    partial class ProcedureControl
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInProcedure = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnInOutProcedure = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInProcedure
+            // 
+            this.btnInProcedure.Location = new System.Drawing.Point(15, 24);
+            this.btnInProcedure.Name = "btnInProcedure";
+            this.btnInProcedure.Size = new System.Drawing.Size(135, 36);
+            this.btnInProcedure.TabIndex = 1;
+            this.btnInProcedure.Text = "带入参数存过";
+            this.btnInProcedure.UseVisualStyleBackColor = true;
+            this.btnInProcedure.Click += new System.EventHandler(this.btnInProcedure_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 83);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(932, 455);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnInOutProcedure
+            // 
+            this.btnInOutProcedure.Location = new System.Drawing.Point(157, 24);
+            this.btnInOutProcedure.Name = "btnInOutProcedure";
+            this.btnInOutProcedure.Size = new System.Drawing.Size(164, 36);
+            this.btnInOutProcedure.TabIndex = 3;
+            this.btnInOutProcedure.Text = "带入带出参数存过";
+            this.btnInOutProcedure.UseVisualStyleBackColor = true;
+            this.btnInOutProcedure.Click += new System.EventHandler(this.btnInOutProcedure_Click);
+            // 
+            // ProcedureControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnInOutProcedure);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnInProcedure);
+            this.Name = "ProcedureControl";
+            this.Size = new System.Drawing.Size(964, 554);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnInProcedure;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnInOutProcedure;
+    }
+}
---
+++ SqlExercise/Procedure/ProcedureControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlExercise.Procedure
+{
+    public partial class ProcedureControl : UserControl
+    {
+        public ProcedureControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInProcedure_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbuilder = new StringBuilder();
+            sbuilder.Append("use pubs\r\n");
+            sbuilder.Append("go\r\n");
+            sbuilder.Append("create proc P_Titles_ByTitleID_SelectPrice\r\n");
+            sbuilder.Append("@title_id varchar(6)\r\n");
+            sbuilder.Append("as\r\n");
+            sbuilder.Append("select price from titles where title_id=@title_id\r\n");
+            sbuilder.Append("执行\r\n");
+            sbuilder.Append("exec P_Titles_ByTitleID_SelectPrice 'BU1032'\r\n");
+            this.textBox1.Text = sbuilder.ToString();
+        }
+
+        private void btnInOutProcedure_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbuilder = new StringBuilder();
+            sbuilder.Append("create proc P_Titles_ByTitleID_SelectPrice\r\n");
+            sbuilder.Append("@title_id varchar(6),--入参\r\n");
+            sbuilder.Append("@price money output  --出参【出参加标识(output)】\r\n");
+            sbuilder.Append("as\r\n");
+            sbuilder.Append("select @price=price from titles where \r\n");
+            sbuilder.Append("title_id=@title_id\r\n");
+            sbuilder.Append("出参的@在=左边");
+            sbuilder.Append("执行\r\n");
+            sbuilder.Append("declare @price2 money--先声明变量\r\n");
+            sbuilder.Append("exec P_Titles_ByTitleID_SelectPrice--之后再调用\r\n");
+            sbuilder.Append("@title_id='BU1032'\r\n");
+            sbuilder.Append("@price=@price2 output\r\n");
+            sbuilder.Append("select @price2---再之后在查声明变量\r\n");
+            sbuilder.Append("出参要声明，配参后面要加output标识，之后再查声明变量。");
+            this.textBox1.Text = sbuilder.ToString();
+
+        }
+    }
+}
